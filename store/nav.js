f9ce510cb451b946b9f0cac80fbe4cbed3cd1740
theokyle <theokyle@gmail.com>
2025-08-28 09:21:13 -0500

    convert to SPA

---
+++ store/nav.js
@@ -0,0 +1,5 @@
+export default [
+  { url: "/tracker", text: "Tracker", faIcon: "fa-solid fa-map" },
+  { url: "/log", text: "Log", faIcon: "fa-solid fa-clipboard-list" },
+  { url: "/journeys", text: "Journeys", faIcon: "fa-solid fa-compass" }
+];
