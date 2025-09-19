81847cdc1bd316ae7ef1c347528733c20b6d8c7c
theokyle <theokyle@gmail.com>
2025-09-19 10:13:16 -0500

    add after hook to create journey form

--- store/createJourney.js
+++ store/createJourney.js
@@ -1,3 +1,4 @@
 export default {
-  view: "createJourney"
+  view: "createJourney",
+  currentJourney: {}
 };
--- views/createJourney.js
+++ views/createJourney.js
@@ -1,4 +1,6 @@
 import html from "html-literal";
+import axios from "axios";
+import * as store from "../store/index.js";
 import journeyForm from "../components/journeyForm";
 import milestoneForm from "../components/milestoneForm";
 
@@ -10,6 +12,28 @@ function render() {
       </div>
     </main>
   `;
+}
+
+async function after(router) {
+  document.getElementById("journeyForm").addEventListener("submit", event => {
+    event.preventDefault();
+
+    const inputList = event.target.elements;
+
+    const requestData = {
+      name: inputList.journeyName.value,
+      universe: inputList.universe.value,
+      description: inputList.description.value
+    };
+
+    axios
+      .post(`${process.env.STEPQUEST_API_URL}/journeys`, requestData)
+      .then(response => {
+        store.createJourney.currentJourney = response.data;
+        router.resolve();
+      })
+      .catch(error => console.log("Error creating journey: ", error));
+  });
 }
 
 export default {
